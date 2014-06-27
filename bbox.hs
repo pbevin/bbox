f060f52b7e6fdf7e65920c27c12239267736e4b6
Pete Bevin <pete@petebevin.com>
2014-06-26 21:12:20 -0400

    Add "nl" command

--- bbox.hs
+++ bbox.hs
@@ -1,14 +1,21 @@
 import System.Environment
 import Control.Monad
 
-
 cat :: [FilePath] -> IO String
 cat [] = getContents
 cat files = (sequence $ map readFile files) >>= return . concat
 
-tac :: [FilePath] -> IO String
-tac files = (cat files) >>= return . (unlines . reverse . lines)
+processLines :: ([String] -> [String]) -> [FilePath] -> IO String
+processLines f files = (cat files) >>= return . (unlines . f . lines)
 
+tac :: [FilePath] -> IO String
+tac = processLines reverse
+
+nl :: [FilePath] -> IO String
+nl = processLines $ addNumber
+  where addNumber lines = zipWith paste [1..] lines
+        paste lineNo line = (width 6 $ show lineNo) ++ "\t" ++ line
+        width w txt = (replicate (w-length txt) ' ') ++ txt
 
 main :: IO ()
 main = do
@@ -16,3 +23,4 @@ main = do
   case head args of
     "cat" -> (cat $ tail args) >>= putStr
     "tac" -> (tac $ tail args) >>= putStr
+    "nl"  -> (nl  $ tail args) >>= putStr
