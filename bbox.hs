534f0089b8671e1683a8a426dd9069dbfa5154b7
Pete Bevin <pete@petebevin.com>
2014-06-29 12:10:19 -0400

    fmt and head commands

--- bbox.hs
+++ bbox.hs
@@ -1,26 +1,55 @@
 import System.Environment
 import Control.Monad
+import Text.Regex (splitRegex, mkRegex)
+import Data.List
+import qualified Data.Text as T
 
 cat :: [FilePath] -> IO String
 cat [] = getContents
 cat files = (sequence $ map readFile files) >>= return . concat
 
 processLines :: ([String] -> [String]) -> [FilePath] -> IO String
-processLines f files = (cat files) >>= return . (unlines . f . lines)
+processLines f files = (cat files) >>= return . unlines . f . lines
 
 tac :: [FilePath] -> IO String
 tac = processLines reverse
 
 nl :: [FilePath] -> IO String
-nl = processLines $ addNumber
+nl = processLines addNumber
   where addNumber lines = zipWith paste [1..] lines
         paste lineNo line = (width 6 $ show lineNo) ++ "\t" ++ line
         width w txt = (replicate (w-length txt) ' ') ++ txt
 
+strip :: String -> String
+strip = T.unpack . T.strip . T.pack
+
+paragraphs :: String -> [String]
+paragraphs = splitRegex (mkRegex "\n\n+")
+
+unparagraphs :: [String] -> String
+unparagraphs = intercalate "\n" . map (addNewline . strip)
+  where addNewline para = para ++ "\n"
+
+fmt :: [FilePath] -> IO String
+fmt files = (cat files) >>= return . unparagraphs . map (unlines . reverse . foldl (addWord 76) [] . words) . paragraphs
+  where addWord n [] word = [ word ]
+        addWord n (line:lines) word
+          | length line + 1 + length word < n = (line ++ " " ++ word) : lines
+          | otherwise = word : line : lines
+
+headCommand :: [FilePath] -> IO String
+headCommand = processLines (take 10)
+
+commands :: [(String, [FilePath] -> IO String)]
+commands = [ ("cat", cat),
+             ("tac", tac),
+             ("nl", nl),
+             ("fmt", fmt),
+             ("head", headCommand) ]
+
 main :: IO ()
 main = do
   args <- getArgs
-  case head args of
-    "cat" -> (cat $ tail args) >>= putStr
-    "tac" -> (tac $ tail args) >>= putStr
-    "nl"  -> (nl  $ tail args) >>= putStr
+  case lookup (head args) commands of
+    Just cmd -> (cmd $ tail args) >>= putStr
+    Nothing -> putStrLn $ "No such command " ++ (head args)
